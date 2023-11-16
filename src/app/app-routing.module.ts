b2cd02925bce90e0791f133863c859b583dcc2be
晉榜_ <gn3410@hotmail.com>
2023-11-17 00:39:03 +0800

    新增套件、劃分子父層關係
    1.加入bootstrap
    2.建立Member&Note子層
    3.子父層架構建立
    4.新增部分註解

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MemberComponent } from './member/member.component';
+import { NoteComponent } from './note/note.component';
 
-const routes: Routes = [];
+//在這邊可以定義路由(跳轉頁面用)
+const routes: Routes = [
+  { path: "", component: MemberComponent },
+  { path: "Member", component: MemberComponent },
+  { path: "Note", component: NoteComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    enableTracing: true,
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,6 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private http: HttpClient) { }
-  title = '第二次測試修改todo';
-  APIresult = '';
-  ngOnInit(): void {
-
-  }
-
-  getAll_btnClick() {
-    console.log("%c Line:16 🍢", "color:#4fff4B");
-    const url = 'http://localhost:60063/api/Users'
-    this.http.get<any>(url).subscribe(resp => {
-      console.log("%c Line:21 🍐", "color:#465975", resp);
-      this.APIresult = JSON.stringify(resp)
-    })
-  }
-  getSingle_btnClick() {
-    console.log("%c Line:26 🍷", "color:#f5ce50");
-    const url = 'http://localhost:60063/api/Users?id=1'
-    this.http.get<any>(url).subscribe(resp => {
-      console.log("%c Line:28 🥪 resp", "color:#fca650", resp);
-      this.APIresult = JSON.stringify(resp)
-    })
-  }
+export class AppComponent {
+  title='Welcome to 無情ㄉ專案機器，認真ㄅ孩子們'
 }
